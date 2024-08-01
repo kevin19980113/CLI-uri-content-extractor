9fd8a85f95b7e7991ccedc869462dbd6db179007
HYUNHO <jeny95kr@gmail.com>
2024-08-01 18:29:12 -0400

    updated

--- README.md
+++ README.md
@@ -8,6 +8,15 @@ A command-line interface tool to extract text content from a webpage using a CSS
 - cheerio
 - commander
 
+# How To Use
+
+1. Install the package
+2. Run the CLI tool with the following command:
+
+- <url>
+- <selector>
+- " extract-content `<url>` `<selector>` "
+
 # Link
 
 - https://www.npmjs.com/package/cli-uri-content-extractor
--- index.js
+++ index.js
@@ -2,30 +2,46 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const { program } = require("commander");
 
+async function getContent(uri) {
+  if (uri.startsWith("http://") || uri.startsWith("https://")) {
+    const response = await axios.get(uri); // if input is a URL, get the content(.html) from the URL
+    return response.data;
+  } else {
+    throw new Error("Inappropriate URI. Please provide a valid URI.");
+  }
+}
+
+function extractContent(content, selector) {
+  const $ = cheerio.load(content); // load HTML content into cheerio
+  const elements = $(selector);
+  return elements.length > 0
+    ? elements.map((index, element) => $(element).text().trim()).get()
+    : null;
+}
+
 program
   .version("1.0.0")
   .description(
     "A CLI tool to extract text content from a webpage using a CSS selector"
   )
-  .argument("<url>", "URL of the webpage")
-  .argument("<selector>", "CSS selector to match the element") // take arguments from command line
-  .action(async (url, selector) => {
+  .arguments("<uri> <selector>") // take arguments from command line
+  .action(async (uri, selector) => {
     // action to be performed when the right arguments are provided
     try {
-      const response = await axios.get(url); // get URL
-      const $ = cheerio.load(response.data); // load HTML into cheerio
-      const element = $(selector); // select element using CSS selector
+      const content = await getContent(uri);
+      const extractedContent = extractContent(content, selector);
 
-      if (element.length === 0) {
-        console.error("No element found matching the given selector.");
-        process.exit(1); // exit if no element found
+      if (extractedContent) {
+        // check if content is not null
+        console.log(extractedContent);
+      } else {
+        console.error("No content found matching the given selector.");
+        process.exit(1);
       }
-
-      console.log(element.text().trim()); // print text content of the selected element
     } catch (error) {
-      console.error("An error occurred:", error.message);
+      console.error("An error occurred :", error.message);
       process.exit(1);
     }
   });
 
-program.parse(process.argv); // start parsing command-line arguments
+program.parse(process.argv);
